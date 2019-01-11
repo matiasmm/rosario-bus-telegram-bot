511bbc89589e73b837ad382ce1f535954e473487
Ezequiel Pochiero <epochiero@gmail.com>
2019-01-11 18:28:37 -0300

    Arrow function

--- src/dialogs/CuantoTengoDialog.js
+++ src/dialogs/CuantoTengoDialog.js
@@ -4,10 +4,10 @@ import Dialog from './Dialog';
 class CuantoTengoDialog extends Dialog {
   onMessage(msg, match) {
     const chat_id = msg.from.id;
-    get_user_info(chat_id, function(results) {
+    get_user_info(chat_id, (results) => {
         const dni = results[0].dni.toString();
         const nro_tarjeta = results[0].nro_tarjeta.toString();
-        get_cuanto_tengo(dni, nro_tarjeta, function(message) {
+        get_cuanto_tengo(dni, nro_tarjeta, (message) => {
             this.bot.sendMessage(chat_id, message, {parse_mode: 'Markdown'});
         });
     });
