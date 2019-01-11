9a7886e372c5d0d26e75aa926f308b79ea7f2c5a
Ezequiel Pochiero <epochiero@gmail.com>
2019-01-11 18:16:00 -0300

    Implement CuantoTengoDialog

---
+++ src/dialogs/CuantoTengoDialog.js
@@ -0,0 +1,17 @@
+import { get_cuanto_tengo, get_user_info } from '../cuanto_tengo/helpers';
+import Dialog from './Dialog';
+
+class CuantoTengoDialog extends Dialog {
+  onMessage(msg, match) {
+    const chat_id = msg.from.id;
+    get_user_info(chat_id, function(results) {
+        const dni = results[0].dni.toString();
+        const nro_tarjeta = results[0].nro_tarjeta.toString();
+        get_cuanto_tengo(dni, nro_tarjeta, function(message) {
+            bot.sendMessage(chat_id, message, {parse_mode: 'Markdown'});
+        });
+    });
+  }
+}
+
+export default CuantoTengoDialog;
--- src/start.js
+++ src/start.js
@@ -1,29 +1,13 @@
-import { get_cuanto_tengo, get_user_info, save_user_info } from './cuanto_tengo/helpers';
+import { save_user_info } from './cuanto_tengo/helpers';
 
 import CuandoDialog from './dialogs/CuandoDialog';
+import CuantoTengoDialog from './dialogs/CuantoTengoDialog';
 
 const TelegramBot = require('node-telegram-bot-api');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(BOT_TOKEN, {polling: true});
-
-// Matches "/cuanto"
-bot.onText(/\/cuanto/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  const chat_id = msg.from.id;
-  get_user_info(chat_id, function(results) {
-    const dni = results[0].dni.toString();
-    const nro_tarjeta = results[0].nro_tarjeta.toString();
-    get_cuanto_tengo(dni, nro_tarjeta, function(message) {
-      // send back the matched "whatever" to the chat
-      bot.sendMessage(chat_id, message, {parse_mode: 'Markdown'});
-    });
-  });
-});
 
 // Matches "/registro"
 bot.onText(/\/registro (\d+) (\d+)/, (msg, match) => {
@@ -43,9 +27,9 @@ bot.onText(/\/registro (\d+) (\d+)/, (msg, match) => {
   });
 });
 
-
-
 new CuandoDialog(bot, /\/cuando (.+)/)
+
+new CuantoTengoDialog(bot, /\/cuanto/)
 
 // bot.on('message', (ctx) => console.log(ctx))
 bot.on('polling_error', (error) => {
