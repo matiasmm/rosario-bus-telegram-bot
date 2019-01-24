38bca0cffde85505f61d66c04551882a1252a307
Ezequiel Pochiero <epochiero@gmail.com>
2019-01-24 18:56:10 -0300

    test commit

--- src/dialogs/CuandoDialog.js
+++ src/dialogs/CuandoDialog.js
@@ -45,8 +45,9 @@ class CuandoDialog extends Dialog {
     }
   }
 
+
   async onCallback(msg, data) {
-    console.log(49, msg);
+    console.log(50, msg);
     const chat_id = msg.from.id;
     const response = await cuandoLlega(data.p, data.i, data.v)
     const messages = response.map(({ text, arribo }) => `*${text}:* ${arribo} `);
