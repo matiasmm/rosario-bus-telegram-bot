e93ae5b4c1914212622e6f3081924c09b6e441d7
Ezequiel Pochiero <epochiero@gmail.com>
2019-03-18 19:46:26 -0300

    Catch exception on CuantoTengoDialog

--- src/dialogs/CuantoTengoDialog.js
+++ src/dialogs/CuantoTengoDialog.js
@@ -6,17 +6,21 @@ class CuantoTengoDialog extends Dialog {
     const chat_id = msg.from.id;
     console.log(`chat_id = ${chat_id}`);
     get_user_info(chat_id, (user) => {
-        const dni = user.dni.toString();
-        const nro_tarjeta = user.nro_tarjeta.toString();
-        get_cuanto_tengo(dni, nro_tarjeta, (response) => {
-          var message;
-          if ('error' in response) {
-            message = response['error'];
-          } else {
-            var message = `Che, al *${response['fecha']} ${response['hora']}* tenés *$${response['saldo']}* de saldo.`;
-          }
+        try {
+          const dni = user.dni.toString();
+          const nro_tarjeta = user.nro_tarjeta.toString();
+          get_cuanto_tengo(dni, nro_tarjeta, (response) => {
+            var message;
+            if ('error' in response) {
+              message = response['error'];
+            } else {
+              var message = `Che, al *${response['fecha']} ${response['hora']}* tenés *$${response['saldo']}* de saldo.`;
+            }
             this.bot.sendMessage(chat_id, message, {parse_mode: 'Markdown'});
         });
+      } catch(ex) {
+          console.log(ex);
+      }
     });
   }
 }
