653fd3029e339bbde039614d5aa9c2c54718b943
Ezequiel Pochiero <epochiero@gmail.com>
2019-04-09 15:21:54 -0300

    Add ability to search by nro_parada

--- src/queries/byEsquina.js
+++ src/queries/byEsquina.js
@@ -1,4 +1,9 @@
-import { byEsquina } from '../helpers/paradasLoaders';
+import { byEsquina, byParada } from '../helpers/paradasLoaders';
+
+export function searchParada(str) {
+  const result = byParada(str);
+  return result;
+}
 
 export function searchEsquina(str) {
   const result = byEsquina(str);
