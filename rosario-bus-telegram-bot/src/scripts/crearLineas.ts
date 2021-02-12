16be96c9dc48cde020ea6ff4cfad0ffa8c1ace8e
Matias M. Montenegro <matias@analyticsfire.com>
2021-02-12 13:46:58 -0300

    crearLineas working

--- rosario-bus-telegram-bot/src/scripts/crearLineas.ts
+++ rosario-bus-telegram-bot/src/scripts/crearLineas.ts
@@ -1,11 +1,9 @@
-import {
-  filter, map, pipe, prop, propIs,
-} from 'ramda';
+import { filter, map, pipe, prop, propIs } from 'ramda';
 import { load as cheerioLoad } from 'cheerio';
 import axios from 'axios';
 import { urlParadas } from '../urls';
 import { Linea } from '../models';
-import { groupsOf } from '../utils/ramdaUtils'
+import { groupsOf } from '../utils/ramdaUtils';
 
 interface ILinea {
   id: number;
@@ -15,32 +13,31 @@ interface ILinea {
   text: string;
 }
 
-const lineasFromOptions = ($: cheerio.Root) => pipe(
-  map($),
-  map(($elem: cheerio.Cheerio) => ({
-    identidad: parseInt($elem.prop('identidad')),
-    bandera: $elem.prop('bandera'),
-    idlinea: parseInt($elem.prop('idlinea')),
-    text: $elem.text(),
-    id: parseInt($elem.val()),
-  })),
-  filter(propIs(String, 'idlinea')),
-)($('#linea option').toArray());
+const lineasFromOptions = ($: cheerio.Root) =>
+  pipe(
+    map($),
+    map(($elem: cheerio.Cheerio) => ({
+      identidad: parseInt($elem.prop('identidad')),
+      bandera: $elem.prop('bandera'),
+      idlinea: parseInt($elem.prop('idlinea')),
+      text: $elem.text(),
+      id: parseInt($elem.val()),
+    })),
+    filter(propIs(String, 'bandera'))
+  )($('#linea option').toArray());
 
-const bodyToObjects = pipe(
-  prop('data'), 
-  cheerioLoad, 
-  lineasFromOptions);
+const bodyToObjects = pipe(prop('data'), cheerioLoad, lineasFromOptions);
 
 export default async function crearLineas() {
   const body = await axios.get(urlParadas);
   const lineas: ILinea[] = bodyToObjects(body);
 
+  console.log('Insertando lineas', lineas.length);
   for (const batch of groupsOf(20, lineas)) {
-    try{
-       await Linea.batchPut(batch);
-    } catch(error) {
-      console.error("Not able to insert lineas")
+    try {
+      await Linea.batchPut(batch);
+    } catch (error) {
+      console.error('No se pudo insertar lineas');
       throw error;
     }
   }
